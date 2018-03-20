de01d8b56261225a94ff76996e77eacc814bbfec
vagrant <vagrant@homestead>
2018-03-20 14:37:39 +0000

    WYSIWYG Editor

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
     <title>@yield('title', 'LaraBBS') - Laravel 进阶教程</title>
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @yield('styles')
 </head>
 
 <body>
@@ -23,6 +24,7 @@
     </div>
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
+    @yield('scripts')
 </body>
 
 </html>
