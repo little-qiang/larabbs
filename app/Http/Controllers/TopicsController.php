036a1b5adf449954a747b11c29154d3cdde2d9d5
vagrant <vagrant@homestead>
2018-03-20 15:17:39 +0000

    Users can edit their own topic

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -45,7 +45,6 @@ class TopicsController extends Controller {
 	public function update(TopicRequest $request, Topic $topic) {
 		$this->authorize('update', $topic);
 		$topic->update($request->all());
-
 		return redirect()->route('topics.show', $topic->id)->with('message', 'Updated successfully.');
 	}
 
