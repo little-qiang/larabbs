a166297514b3d17fd8d36cad0d186c4bec27cf01
little_qiang <346669517@qq.com>
2018-03-26 03:28:43 +0000

    Sidebar active users

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -9,15 +9,17 @@ use App\Models\Category;
 use Illuminate\Http\Request;
 use Auth;
 use App\Handlers\ImageUploadHandler;
+use App\Models\User;
 
 class TopicsController extends Controller {
 	public function __construct() {
 		$this->middleware('auth', ['except' => ['index', 'show']]);
 	}
 
-	public function index(Request $request, Topic $topic) {
+	public function index(Request $request, Topic $topic, User $user) {
         $topics = $topic->withOrder($request->order)->paginate(20);
-		return view('topics.index', compact('topics'));
+        $active_users = $user->getActiveUsers();
+		return view('topics.index', compact('topics', 'active_users'));
 	}
 
 	public function show(Request $request, Topic $topic) {
