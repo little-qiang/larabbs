50f08e6ab26ac43cf0d88a7efb985732447aa310
vagrant <vagrant@homestead>
2018-04-17 10:55:52 +0000

    locale

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -39,7 +39,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            'throttle:60,1',
+            'throttle:10,1',
             'bindings',
         ],
     ];
@@ -58,5 +58,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'change-locale' => \App\Http\Middleware\ChangeLocale::class,
     ];
 }
